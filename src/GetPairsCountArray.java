f41810cd1675788e07e64b302168a2cd22c64d41
Kunal Kheeva <98793512+kunalkheeva1@users.noreply.github.com>
2022-06-25 23:04:44 -0700

    created a method to count the number of pairs in array to meet the requirement of given integer

--- src/GetPairsCountArray.java
+++ src/GetPairsCountArray.java
@@ -1,5 +1,18 @@
+import java.util.HashMap;
+
 public class GetPairsCountArray {
-   public static
+   public static int getPairsCountArray(int arr[], int n, int k){
+       HashMap<Integer, Integer> map = new HashMap<>();
+       int count = 0;
+       for(int i= 0; i<n; i++){
+           if(map.containsKey(k-arr[i])){
+               count = count + map.get(k-arr[i]);
+           }if(map.containsKey(arr[i])){
+               map.put(arr[i],map.get(arr[i])+1);
+           }
+           else map.put(arr[i], 1);
+       }return count;
+   }
 
     public static void main(String[] args) {
 
